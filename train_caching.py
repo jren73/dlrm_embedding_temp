0aeb455ff3f539720118a0b04bb76394784d4ec0
jren73 <renjie73@outlook.com>
2023-02-13 02:30:28 +0000

    update

--- train_caching.py
+++ train_caching.py
@@ -135,63 +135,6 @@ def train_model(model, train_set,eval_set,seq_length, n_epochs):
         f.close()
         return model.eval()
 
-def train(model, optimizer, train_loader, state):
-    epoch, n_epochs, train_steps = state
-
-    losses = []
-    cers = []
-
-    # t = tqdm.tqdm(total=min(len(train_loader), train_steps))
-    t = tqdm.tqdm(train_loader)
-    model.train()
-
-    for batch in t:
-        t.set_description("Epoch {:.0f}/{:.0f} (train={})".format(epoch, n_epochs, model.training))
-        loss, _, _, _ = model.loss(batch)
-        losses.append(loss.item())
-        # Reset gradients
-        optimizer.zero_grad()
-        # Compute gradients
-        loss.backward()
-        # torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=2)
-        optimizer.step()
-        t.set_postfix(loss='{:05.3f}'.format(loss.item()), avg_loss='{:05.3f}'.format(np.mean(losses)))
-        t.update()
-
-    return model, optimizer
-    # print(" End of training:  loss={:05.3f} , cer={:03.1f}".format(np.mean(losses), np.mean(cers)*100))
-
-
-'''
-def evaluate(model, eval_loader):
-
-    losses = []
-    accs = []
-
-    t = tqdm.tqdm(eval_loader)
-    model.eval()
-
-    with torch.no_grad():
-        for batch in t:
-            t.set_description(" Evaluating... (train={})".format(model.training))
-            loss, logits, labels, alignments = model.loss(batch)
-            preds = logits.detach().cpu().numpy()
-            # acc = np.sum(np.argmax(preds, -1) == labels.detach().cpu().numpy()) / len(preds)
-            acc = 100 * editdistance.eval(np.argmax(preds, -1), labels.detach().cpu().numpy()) / len(preds)
-            losses.append(loss.item())
-            accs.append(acc)
-            t.set_postfix(avg_acc='{:05.3f}'.format(np.mean(accs)), avg_loss='{:05.3f}'.format(np.mean(losses)))
-            t.update()
-        align = alignments.detach().cpu().numpy()[:, :, 0]
-
-    # Uncomment if you want to visualise weights
-    # fig, ax = plt.subplots(1, 1)
-    # ax.pcolormesh(align)
-    # fig.savefig("data/att.png")
-    print("  End of evaluation : loss {:05.3f} , acc {:03.1f}".format(np.mean(losses), np.mean(accs)))
-    # return {'loss': np.mean(losses), 'cer': np.mean(accs)*100}
-'''
-
 def run(traceFile, model_type):
     #USE_CUDA = 0
     config_path = FLAGS.config
@@ -201,18 +144,19 @@ def run(traceFile, model_type):
 
     with open(config_path, "r") as f:
         config = json.load(f)
+
     config["gpu"] = torch.cuda.is_available()
     input_sequence_length = config["n_channels"]
     evaluation_windown_length = config["evaluation_window"]
     BATCHSIZE = config["batch_size"]
 
+    model = Seq2Seq_cache(input_sequence_length, 1, 512, input_sequence_length)
+    model = model.to(device)
+    print(model)
+
     if model_type == 1:
-        model = seq2seq_prefetch(config)
+        model.load_state_dict(torch.load('models/cache_model.pt'))
     else:
-        model = Seq2Seq_cache(input_sequence_length, 1, 512, input_sequence_length)
-                               
-        model = model.to(device)
-        print(model)
         model.apply(init_weights)
 
     inputsfolder = traceFile
@@ -332,9 +276,9 @@ if __name__ == '__main__':
     parser.add_argument('--config', type=str)
     parser.add_argument('--epochs', default=1, type=int)
     parser.add_argument('--traceFile', type=str,  help='trace file name\n')
-    parser.add_argument('--model_type', default=0, type=int,  help='0 for training from stratch, 1 for loading saved model\n')    parser.add_argument('--infFile', type=str,  help='inference file name\n')
+    parser.add_argument('--model_type', default=0, type=int,  help='0 for training from stratch, 1 for loading saved model\n')    
     parser.add_argument('--infFile', type=str,  help='inference file name\n')
-
+    
     FLAGS, _ = parser.parse_known_args()
     traceFile = FLAGS.traceFile
     model_type = FLAGS.model_type
